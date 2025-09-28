5e885dfaaac623beb7825530ac7809263150653a
kvnbanunu <kvnbanunu@gmail.com>
2025-09-27 22:24:49 -0700

    Progress on cipher functions

--- internal/vigenere.go
+++ internal/vigenere.go
@@ -1,19 +1,53 @@
 package internal
 
+import (
+	"log"
+	"unicode"
+)
+
 // Holds the message to be ciphered/deciphered w/ key
 type Msg struct {
 	Content string `json:"content"`
 	Key     string `json:"key"`
 }
 
-func Cipher() {
+func cipher(c rune, shift rune) string {
+	if c > 'a' && c < 'z' {
+		c += shift % 32
+	} else {
+
+	}
+}
+
+func decipher(c rune, shift rune) string {
 
 }
 
-func DeCipher() {
+func Process(m Msg, task string) string {
 
-}
+	// Keep appending the key to itself until it is at least the length of the content
+	for len(m.Key) < len(m.Content) {
+		m.Key += m.Key
+	}
 
-func Process(str string) string {
-	return str
+	keyIndex := 0
+	output := ""
+
+	for _, c := range(m.Content) {
+		if !unicode.IsLetter(c) {
+			output += string(c)
+			continue
+		}
+		shift := rune(m.Key[keyIndex])
+		switch task {
+		case "cipher":
+			output += cipher(c, shift)
+		case "decipher":
+			output += decipher(c, shift)
+		default: // should never reach here
+			log.Fatalf("Invalid task: %s", task)
+		}
+		keyIndex++
+	}
+	return output
 }
